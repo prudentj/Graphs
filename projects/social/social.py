049994e40835cf0dfaa4816be14b77485fc504ca
Jacob Prudent <jake.prudent@gmail.com>
2020-06-10 15:59:17 -0700

    MVP

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,28 @@
+import random
+# copy and paste random class
+
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -29,24 +51,28 @@ class SocialGraph:
         self.friendships[self.last_id] = set()
 
     def populate_graph(self, num_users, avg_friendships):
-        """
-        Takes a number of users and an average number of friendships
-        as arguments
-
-        Creates that number of users and a randomly distributed friendships
-        between those users.
-
-        The number of users must be greater than the average number of friendships.
-        """
         # Reset graph
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
-
         # Add users
-
-        # Create friendships
+        for i in range(0, num_users):
+            self.add_user(f"User {i}")
+            # Create Frienships
+        # Generate all possible friendship combinations
+        possible_friendships = []
+        # Avoid duplicates by ensuring the first number is smaller than the second
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+        # Shuffle the possible friendships
+        random.shuffle(possible_friendships)
+        # Create friendships for the first X pairs of the list
+        # X is determined by the formula: num_users * avg_friendships // 2
+        # Need to divide by 2 since each add_friendship() creates 2 friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,6 +85,33 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        paths_walked = Queue()
+        paths_walked.enqueue([user_id])
+        # need_to_visit=Queue()
+        # need_to_visit.enqueue(user_id)
+        # traverse the whole graph breadth search
+        # if the node hasn't been added to the visited add it(don't worry otherwise it isn't the )
+        while paths_walked.size() > 0:
+            path = paths_walked.dequeue()
+            current_friend = path[-1]
+
+            if current_friend in visited:
+                # stops this iteration. The loop will continue with next path
+                continue
+            # add it to my list of visited nodes
+            visited[current_friend] = path
+            # lets look at all the friends that friend has and if we haven't visited them
+
+            for friend in self.friendships[current_friend]:
+                path_copy = path.copy()
+                path_copy.append(friend)
+                paths_walked.enqueue(path_copy)
+
+                # Maybe my queue stores paths walked up to that point.
+                # The last index is the friend I need to investigate.
+                # if that friend has no friends then it will not be enqueued
+                # if it does I add that friend to the visited list append it to the list and add it to they queue
+
         return visited
 
 
